521428c09f9b27a5fc5f76b7c4ff3fee96a82e4b
WIN 10 Desktop GVR <gsenandika@gmail.com>
2024-03-21 15:07:54 +0700

    change resources dir

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,41 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import path from "path";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+export default defineConfig(() => {
+	const env = loadEnv(null, process.cwd());
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+	return {
+		plugins: [
+            vue(), 
+            basicSsl(),
+        ],
+		build: {
+			emptyOutDir: false,
+            copyPublicDir: false,
+			outDir: 'public',
+			assetsDir: env.VITE_ASSETS_DIR,
+			manifest: true,
+			rollupOptions: {
+				input: `./${env.VITE_RESOURCES_DIR}/${env.VITE_ENTRY_FILE}`,
+			},
+		},
+		server: {
+			origin: env.VITE_ORIGIN,
+			port: env.VITE_PORT,
+			strictPort: true,
+            https: true,
+            hmr: {
+                host: env.VITE_HOST,
+                port: env.VITE_PORT,
+                protocol: 'wss'
+            }
+		},
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, `./${env.VITE_RESOURCES_DIR}`),
+			},
+		},
+	};
+});
