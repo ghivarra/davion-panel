93760f2757a9ea86df75b19dfa6629cebecaf3d3
WIN 10 Desktop GVR <gsenandika@gmail.com>
2024-03-23 03:41:00 +0700

    creating Admin Role table for ORM Model

---
+++ app/Models/AdminRole.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class AdminRole extends Model
+{
+    protected $table            = 'admin_role';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'name', 'is_superadmin', 'status'
+    ];
+
+    protected bool $allowEmptyInserts = false;
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
