cfbc2f4eb2a3304aa28e6151fde929143303c2f5
WIN 10 Desktop GVR <gsenandika@gmail.com>
2024-03-23 02:22:21 +0700

    creating Admin table for ORM Model

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,19 +2,14 @@
 
 namespace App\Controllers;
 
-use App\Libraries\Ghivarra\VueIgniter;
-
 class Home extends BaseController
 {
     public function index(): string
     {
-        $db = db_connect();
-        dd($db->DBDriver);
-
-        $vueIgniter = new VueIgniter();
-        return $vueIgniter->render('MyComponent', [
+        return $this->vue->render('MyComponent', [
             'ucing' => 'oyok',
             'myman' => 'pei',
+            'haha' => 'hehehee'
         ]);
     }
 }
---
+++ app/Models/AdminModel.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class AdminModel extends Model
+{
+    protected $table            = 'admin';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'username', 'password', 'name', 'email', 'email_verified_at', 'status', 'admin_role_id', 'photo', 'token', 'token_expired_at'
+    ];
+
+    protected bool $allowEmptyInserts = false;
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
