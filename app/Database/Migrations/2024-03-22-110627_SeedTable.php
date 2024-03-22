3de25f0f46128e204759925e7357e4001e113e6f
WIN 10 Desktop GVR <gsenandika@gmail.com>
2024-03-22 18:25:31 +0700

    create seed menu

--- app/Database/Migrations/2024-03-22-110627_SeedTable.php
+++ app/Database/Migrations/2024-03-22-110627_SeedTable.php
@@ -12,6 +12,7 @@ class SeedTable extends Migration
     {
         $this->seedWebsite();
         $this->seedAdmin();
+        $this->seedMenu();
     }
 
     //=====================================================================================================
@@ -68,6 +69,110 @@ class SeedTable extends Migration
             'admin_role_id'     => $roleId,
             'created_at'        => $now,
             'updated_at'        => $now
+        ]);
+    }
+
+    //=====================================================================================================
+
+    public function seedMenu()
+    {
+        $db  = Database::connect();
+        $now = date('Y-m-d H:i:s');
+
+        // create menu group
+        $db->table("admin_menu_group")->insert([
+            'name'       => 'Pengaturan',
+            'sort_order' => 1,
+            'status'     => 'Aktif',
+            'created_at' => $now,
+            'updated_at' => $now
+        ]);
+        $groupId = $db->insertId();
+
+        // add menu
+        $db->table('admin_menu')->insertBatch([
+            [
+                'title'               => 'Dasbor', 
+                'router_name'         => 'panel.dashboard', 
+                'icon'                => 'fa-solid fa-table-cells-large',
+                'type'                => 'Primary',
+                'sort_order'          => 1,
+                'created_at'          => $now, 
+                'updated_at'          => $now,
+                'admin_menu_group_id' => NULL
+            ],[
+                'title'               => 'Profil', 
+                'router_name'         => 'panel.profile', 
+                'icon'                => 'fa-solid fa-user',
+                'type'                => 'Primary',
+                'sort_order'          => 2,
+                'created_at'          => $now, 
+                'updated_at'          => $now,
+                'admin_menu_group_id' => NULL
+            ], [
+                'title'               => 'Admin', 
+                'router_name'         => NULL,
+                'icon'                => 'fa-solid fa-user-tie',
+                'type'                => 'Parent',
+                'sort_order'          => 1,
+                'created_at'          => $now, 
+                'updated_at'          => $now,
+                'admin_menu_group_id' => $groupId,
+            ], [
+                'title'               => 'Akun', 
+                'router_name'         => 'panel.admin',
+                'icon'                => NULL,
+                'type'                => 'Child',
+                'sort_order'          => 1,
+                'created_at'          => $now, 
+                'updated_at'          => $now,
+                'admin_menu_group_id' => NULL,
+            ], [
+                'title'               => 'Role', 
+                'router_name'         => 'panel.role',
+                'icon'                => NULL,
+                'type'                => 'Child',
+                'sort_order'          => 2,
+                'created_at'          => $now, 
+                'updated_at'          => $now,
+                'admin_menu_group_id' => NULL,
+            ], [
+                'title'               => 'Halaman', 
+                'router_name'         => NULL,
+                'icon'                => 'fa-solid fa-table-columns',
+                'type'                => 'Parent',
+                'sort_order'          => 2,
+                'created_at'          => $now, 
+                'updated_at'          => $now,
+                'admin_menu_group_id' => $groupId,
+            ], [
+                'title'               => 'Modul', 
+                'router_name'         => 'panel.module',
+                'icon'                => NULL,
+                'type'                => 'Child',
+                'sort_order'          => 1,
+                'created_at'          => $now, 
+                'updated_at'          => $now,
+                'admin_menu_group_id' => NULL,
+            ], [
+                'title'               => 'Role', 
+                'router_name'         => 'panel.menu',
+                'icon'                => NULL,
+                'type'                => 'Child',
+                'sort_order'          => 2,
+                'created_at'          => $now, 
+                'updated_at'          => $now,
+                'admin_menu_group_id' => NULL,
+            ], [
+                'title'               => 'Website', 
+                'router_name'         => 'panel.website',
+                'icon'                => 'fa-solid fa-globe',
+                'type'                => 'Primary',
+                'sort_order'          => 3,
+                'created_at'          => $now, 
+                'updated_at'          => $now,
+                'admin_menu_group_id' => $groupId,
+            ]
         ]);
     }
 
