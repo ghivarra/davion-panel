d86978aebd81575df61863a3ebfd73e0eb52c8c6
WIN 10 Desktop GVR <gsenandika@gmail.com>
2024-03-23 18:08:39 +0700

    add panel routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -12,6 +12,11 @@ $routes->group($_ENV['LOGIN_PAGE'], static function ($routes) {
     $routes->get('/', 'Admin\\LoginController::index', ['as' => 'login']);
 });
 
+// dashboard routes
+$routes->group($_ENV['PANEL_PAGE'], static function ($routes) {
+    $routes->get('/', 'Admin\\DashboardController::index', ['as' => 'panel.dashboard']);
+});
+
 // dynamic image routes
 $routes->group('assets', static function ($routes) {
     $routes->get('images/(:any)', 'Assets\\ImageController::serve/$1');
