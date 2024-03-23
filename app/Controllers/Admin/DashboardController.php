1d22fabf06f4b36229101b582b523f325dc61052
WIN 10 Desktop GVR <gsenandika@gmail.com>
2024-03-23 18:08:53 +0700

    add dashboard controller

---
+++ app/Controllers/Admin/DashboardController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class DashboardController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
--- app/Controllers/Admin/LoginController.php
+++ app/Controllers/Admin/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
+use App\Libraries\Ghivarra\DavionShield;
 use App\Models\WebsiteModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
