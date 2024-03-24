270eb24ee8bc9920dd77b4a6e4ba24ef9e17120f
Ghivarra Senandika R from Laptop <senandika95@gmail.com>
2024-03-25 05:54:00 +0700

    update VueIgniter JS so it is comply with newer Node JS versions

--- resources/libraries/Ghivarra/VueIgniter/index.js
+++ resources/libraries/Ghivarra/VueIgniter/index.js
@@ -1,7 +1,7 @@
 const VueIgniter = (options) => {
     let id = (typeof options.rootId === 'undefined') ? '#app' : `#${options.rootId}`
     const pageData = JSON.parse(document.querySelector(id).getAttribute('data-page'))
-    import(`../../../views/${pageData.view}.vue`).then((app) => {
+    import('../../../views/' + pageData.view + '.vue').then((app) => {
         options.setup(app.default, Object.assign({}, pageData.data), id)
     })
 }
