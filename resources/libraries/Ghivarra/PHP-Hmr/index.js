639d74d38b6ec7dae793864cdf00ccad6ad63454
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-03-27 06:04:30 +0700

    update hot reload rules

--- resources/libraries/Ghivarra/PHP-Hmr/index.js
+++ resources/libraries/Ghivarra/PHP-Hmr/index.js
@@ -1,13 +1,18 @@
+import * as path from 'path';
+
+/* global __dirname */
+
 const customHotReload = () => {
     return {
         name: 'custom-hot-reload',
         enforce: 'post',
         handleHotUpdate: ({file, server}) => {
-            if (file.endsWith('.php')) {
-                console.log('reload PHP file')
+            let rootPath = path.normalize(`${__dirname}/../../../../app/Views`)
+            let filePath = path.normalize(file)
+            if (filePath.endsWith('.php') && filePath.startsWith(rootPath)) {
                 server.ws.send({
                     type: 'full-reload',
-                    path: 'app/*'
+                    path: '*'
                 })
             }
         }
