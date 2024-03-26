e8b1e8e23faab0e1772264527b58dc921a577c3a
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-03-27 02:48:23 +0700

    create routes on menu pages

--- resources/router/index.js
+++ resources/router/index.js
@@ -12,7 +12,12 @@ export default [
 
     // Admin Menu Routes
     { path: `/${env.VITE_PANEL_PAGE}`, name: 'panel.dashboard', component: () => import('../views/Panel/PanelDashboardView.vue'), meta: { 'pageName': 'Dasbor' } },
-
+    { path: `/${env.VITE_PANEL_PAGE}/administrator`, name: 'panel.admin', component: () => import('../views/Panel/PanelAdminView.vue'), meta: { 'pageName': 'Administrator' } },
+    { path: `/${env.VITE_PANEL_PAGE}/role`, name: 'panel.role', component: () => import('../views/Panel/PanelRoleView.vue'), meta: { 'pageName': 'Role & Perizinan' } },
+    { path: `/${env.VITE_PANEL_PAGE}/module`, name: 'panel.module', component: () => import('../views/Panel/PanelModuleView.vue'), meta: { 'pageName': 'Modul & Fitur' } },
+    { path: `/${env.VITE_PANEL_PAGE}/menu`, name: 'panel.menu', component: () => import('../views/Panel/PanelMenuView.vue'), meta: { 'pageName': 'Tampilan Menu' } },
+    { path: `/${env.VITE_PANEL_PAGE}/website`, name: 'panel.website', component: () => import('../views/Panel/PanelWebsiteView.vue'), meta: { 'pageName': 'Pengaturan Website' } },
+    
     // Catch All / 404 Page
     { path: '/:pathMatch(.*)*', name: 'pageNotFound', component: () => import('../components/PageNotFoundComponent.vue'), meta: { 'pageName': 'Halaman Tidak Ditemukan' } },
 ]
