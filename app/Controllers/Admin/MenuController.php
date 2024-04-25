dde6793f1b072ebd481a22b6f8a843164a7e0d02
WIN 10 Desktop GVR <gsenandika@gmail.com>
2024-04-25 22:01:19 +0700

    done backend logic menu/list

--- app/Controllers/Admin/MenuController.php
+++ app/Controllers/Admin/MenuController.php
@@ -4,13 +4,13 @@ namespace App\Controllers\Admin;
 
 use CodeIgniter\HTTP\ResponseInterface;
 use App\Controllers\BaseController;
-use App\Models\AdminMenuGroup;
+use App\Models\AdminMenuGroupModel;
 use App\Models\AdminMenuModel;
 use Config\Services;
 
 class MenuController extends BaseController
 {
-    public function list()
+    public function list(): ResponseInterface
     {
         $permission = $this->checkPermission('menuView');
         
@@ -22,11 +22,55 @@ class MenuController extends BaseController
             ]);
         }
 
-        // get instance
-        $orm = new AdminMenuModel();
+        // get menu groups
+        $adminMenuGroup = new AdminMenuGroupModel();
+        $adminMenu      = new AdminMenuModel();
+        $menuGroup      = $adminMenuGroup->select(['id', 'name', 'status'])->orderBy('sort_order', 'ASC')->find();
 
-        // get
+        // get all menu
+        foreach ($menuGroup as $n => $item):
 
+            $menus = $adminMenu->select(['id', 'title', 'icon', 'type', 'status'])
+                               ->where('admin_menu_group_id', $item['id'])
+                               ->where('type !=', 'Child')
+                               ->orderBy('sort_order', 'ASC')
+                               ->find();
+
+            if (empty($menus))
+            {
+                // set empty child
+                $menuGroup[$n]['child'] = [];
+                continue;
+            }
+
+            foreach ($menus as $i => $menu)
+            {
+                if ($menu['type'] === 'Parent')
+                {
+                    $childs = $adminMenu->select(['id', 'title', 'icon', 'status'])
+                                        ->where('admin_menu_parent_id', $menu['id'])
+                                        ->orderBy('sort_order', 'ASC')
+                                        ->find();
+                    
+                    $menus[$i]['child'] = empty($childs) ? [] : $childs;
+
+                } else {
+
+                    $menus[$i]['child'] = [];
+                }
+            }
+
+            // set child
+            $menuGroup[$n]['child'] = $menus;
+
+        endforeach;
+
+        // return
+        return $this->response->setJSON([
+            'status'  => 'success',
+            'message' => 'Data berhasil ditarik',
+            'data'    => $menuGroup
+        ]);
     }
 
     //================================================================================================
