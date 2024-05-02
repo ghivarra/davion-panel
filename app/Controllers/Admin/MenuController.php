9cb6bd109615b5516430c834e2f15ab67afa418e
WIN 10 Desktop GVR <gsenandika@gmail.com>
2024-05-02 17:38:36 +0700

    done backend menu/group/update-status

--- app/Controllers/Admin/MenuController.php
+++ app/Controllers/Admin/MenuController.php
@@ -196,4 +196,56 @@ class MenuController extends BaseController
     }
 
     //================================================================================================
+
+    public function groupUpdateStatus(): ResponseInterface
+    {
+        $permission = $this->checkPermission('menuUpdate');
+        
+        if (!$permission)
+        {
+            return $this->response->setStatusCode(403)->setJSON([
+                'status'  => 'error',
+                'message' => 'Anda tidak memiliki izin untuk mengakses halaman ini'
+            ]);
+        }
+
+        // validate data
+        $rules = [
+            'id'     => ['label' => 'Grup Menu', 'rules' => 'required|numeric|is_not_unique[admin_menu_group.id]'],
+            'status' => ['label' => 'Status', 'rules' => 'required|in_list[Aktif,Nonaktif]'],
+        ];
+
+        $data = $this->request->getPost(array_keys($rules));
+
+        // HARD CODE SO DEFAULT NOT GET DELETED, NONACTIVED, OR CHANGED
+        if (intval($data['id']) === 1)
+        {
+            return $this->response->setStatusCode(403)->setJSON([
+                'status'  => 'error',
+                'message' => 'Anda tidak memiliki izin untuk mengakses halaman ini'
+            ]);
+        }
+
+        if (!$this->validateData($data, $rules))
+        {
+            // return
+            return $this->response->setJSON([
+                'status'  => 'error',
+                'message' => 'Data tidak tervalidasi',
+                'data'    => $this->validator->getErrors()
+            ]);
+        }
+
+        // save
+        $orm = new AdminMenuGroupModel();
+        $orm->save($data);
+
+        // return
+        return $this->response->setJSON([
+            'status'  => 'success',
+            'message' => 'Status data berhasil diperbaharui'
+        ]);
+    }
+
+    //================================================================================================
 }
