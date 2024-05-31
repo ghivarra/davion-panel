9abf17010f808d684ece91942611e450b60c3259
WIN 10 Desktop GVR <gsenandika@gmail.com>
2024-05-31 16:54:53 +0700

    change change password page title

--- resources/router/index.js
+++ resources/router/index.js
@@ -8,7 +8,7 @@ export default [
     // Header Menu Routes
     { path: `/${env.VITE_PANEL_PAGE}/profile`, name: 'panel.profile', component: () => import('../views/Panel/PanelProfileView.vue'), meta: { 'pageName': 'Profil' } },
     { path: `/${env.VITE_PANEL_PAGE}/account`, name: 'panel.account', component: () => import('../views/Panel/PanelAccountView.vue'), meta: { 'pageName': 'Pengaturan Akun' } },
-    { path: `/${env.VITE_PANEL_PAGE}/account/password`, name: 'panel.account.password', component: () => import('../views/Panel/PanelAccountPasswordView.vue'), meta: { 'pageName': 'Ubah Password' } },
+    { path: `/${env.VITE_PANEL_PAGE}/account/password`, name: 'panel.account.password', component: () => import('../views/Panel/PanelAccountPasswordView.vue'), meta: { 'pageName': 'Password' } },
 
     // Admin Menu Routes
     { path: `/${env.VITE_PANEL_PAGE}`, name: 'panel.dashboard', component: () => import('../views/Panel/PanelDashboardView.vue'), meta: { 'pageName': 'Dasbor' } },
