bc9afebe699f4314740325a5bb91d6f204636e42
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-07-12 13:33:54 +0700

    update vueIgniter

--- resources/libraries/Ghivarra/VueIgniter/index.js
+++ resources/libraries/Ghivarra/VueIgniter/index.js
@@ -1,21 +1,24 @@
+const env = import.meta.env
 const VueIgniter = (options) => {
     let id = (typeof options.rootId === 'undefined') ? '#app' : `#${options.rootId}`
     const pageData = JSON.parse(document.querySelector(id).getAttribute('data-page'))
 
-    if (import.meta.env.VITE_MODE === 'chunked') {
+    if (env.VITE_MODE === 'chunked') {
 
-        const allPages = import.meta.glob('../../../views/**/*.vue')
-        const page = allPages[`../../../views/${pageData.view}.vue`]()
+        const allPages = import.meta.glob('@/views/**/*.vue')
+        const name = `/${import.meta.env.VITE_RESOURCES_DIR}/views/${pageData.view}.vue`
+        const page = allPages[name]()
 
         page.then((app) => {
             options.setup(app.default, Object.assign({}, pageData.data), id)
         })
 
-    } else if (import.meta.env.VITE_MODE === 'single') {
+    } else if (env.VITE_MODE === 'single') {
 
-        const allPages = import.meta.glob('../../../views/**/*.vue', { eager: true })
-        const page = allPages[`../../../views/${pageData.view}.vue`]
-
+        const allPages = import.meta.glob('@/views/**/*.vue', { eager: true })        
+        const name = `/${import.meta.env.VITE_RESOURCES_DIR}/views/${pageData.view}.vue`
+        const page = allPages[name]
+        
         options.setup(page.default, Object.assign({}, pageData.data), id)
     }
 }
