143837ebad225195ef193d367a77f5fab90bff52
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-07-12 13:33:40 +0700

    update main.js and implement new router

--- resources/main.js
+++ resources/main.js
@@ -3,30 +3,34 @@ import { createApp, h } from 'vue'
 import { VueIgniter } from './libraries/Ghivarra/VueIgniter'
 import { createRouter, createWebHistory } from 'vue-router'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import indexSingle from './router/index-single'
-import indexChunked from './router/index-chunked'
+import router from './router'
 import axios from 'axios'
 
 // set axios settings
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// load router
-const vueRouter = createRouter({
-    history: createWebHistory(),
-    routes: (import.meta.env === 'single') ? indexSingle : indexChunked
-});
+// launched vue app after dynamic import
+router().then((res) => {
 
-// create new Vue CodeIgniter
-VueIgniter({
-    rootId: import.meta.env.VITE_APP_ID,
-    setup: (App, props, root) => {
-        const app = createApp({
-            render: () => h(App, props)
-        })
-        app.config.unwrapInjectedRef = true
+    // load router
+    const vueRouter = createRouter({
+        history: createWebHistory(),
+        routes: res.default
+    });
+    
+    // create new Vue CodeIgniter
+    VueIgniter({
+        rootId: import.meta.env.VITE_APP_ID,
+        setup: (App, props, root) => {
+            const app = createApp({
+                render: () => h(App, props)
+            })
+            app.config.unwrapInjectedRef = true
+    
+            app.use(vueRouter)
+            app.component('font-awesome', FontAwesomeIcon)
+            app.mount(root)
+        }
+    })
 
-        app.use(vueRouter)
-        app.component('font-awesome', FontAwesomeIcon)
-        app.mount(root)
-    }
 })
