22480f8c9bb3f354a629106a16b069809005c2be
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-07-15 17:46:05 +0700

    update postcss

--- postcss.config.js
+++ postcss.config.js
@@ -30,6 +30,8 @@ export default {
                 ],
                 greedy: [
                     /^modal-/,
+                    /^dropdown-/,
+                    /^form-/,
                 ]
             }
         })
