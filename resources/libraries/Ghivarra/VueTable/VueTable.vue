5b424d5ed30f26bffa67bdab52a699335a110e38
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-09-18 13:23:17 +0700

    add new slots for vuetable

--- resources/libraries/Ghivarra/VueTable/VueTable.vue
+++ resources/libraries/Ghivarra/VueTable/VueTable.vue
@@ -65,8 +65,9 @@
                             <td v-bind:colspan="columns.length">{{ emptyText }}</td>
                         </tr>
                         <tr v-else v-for="(item, n) in response.row" v-bind:key="n">
-                            <td v-for="(column, b) in columns" v-bind:key="b" v-bind:class="column.class"
-                                v-html="item[column.key]"></td>
+                            <slot name="row" v-bind:row-data="item" v-bind:column-data="columns" v-bind:key="n">
+                                <td v-for="(column, b) in columns" v-bind:key="b" v-bind:class="column.class" v-html="item[column.key]"></td>
+                            </slot>
                         </tr>
                     </tbody>
 
