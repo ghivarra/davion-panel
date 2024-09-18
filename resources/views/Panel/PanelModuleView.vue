16d3d88524ea9265b71a395ff86e7810e03e1a3a
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-09-18 14:06:29 +0700

    split module with their modals

--- resources/views/Panel/PanelModuleView.vue
+++ resources/views/Panel/PanelModuleView.vue
@@ -2,129 +2,18 @@
     <main role="main" class="mb-4">
         <slot name="breadcrumb"></slot>
 
-        <section id="create-table" class="mb-4">
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createFormModal">
-                <font-awesome icon="fas fa-plus" class="me-2"></font-awesome>
-                Tambah Modul
-            </button>
-            <div class="modal fade" id="createFormModal" data-bs-backdrop="static" data-bs-keyboard="false"
-                tabindex="-1" aria-labelledby="createFormModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                    <form v-on:submit.prevent="create" method="POST" class="modal-content">
-                        <div class="modal-header bg-primary text-white">
-                            <h1 class="modal-title fs-5" id="createFormModalLabel">Tambah Modul</h1>
-                        </div>
-                        <div class="modal-body">
-                            <div class="mb-3">
-                                <label for="moduleGroup" class="form-label fw-bold">
-                                    Grup
-                                    <span class="text-danger" title="Wajib Diisi">*</span>
-                                </label>
-                                <input list="groupList" type="text" class="form-control" id="moduleGroup" name="group" maxlength="200" required>
-                                <datalist id="groupList">
-                                    <option v-for="(item, n) in groupList" v-bind:key="n" v-bind:value="item">
-                                        {{ item }}
-                                    </option>
-                                </datalist>
-                            </div>
-                            <div class="mb-3">
-                                <label for="moduleAlias" class="form-label fw-bold">
-                                    Alias
-                                    <span class="text-danger" title="Wajib Diisi">*</span>
-                                </label>
-                                <input type="text" class="form-control" id="moduleAlias" name="alias" maxlength="100" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="moduleName" class="form-label fw-bold">
-                                    Nama
-                                    <span class="text-danger" title="Wajib Diisi">*</span>
-                                </label>
-                                <input type="text" class="form-control" id="moduleName" name="name" maxlength="200" autocomplete="off" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="moduleStatus" class="form-label fw-bold">
-                                    Status
-                                    <span class="text-danger" title="Wajib Diisi">*</span>
-                                </label>
-                                <select name="status" id="moduleStatus" class="form-select" required>
-                                    <option value="Aktif">Aktif</option>
-                                    <option value="Nonaktif">Nonaktif</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button ref="createModalCloseBtn" type="button" class="btn btn-secondary"
-                                data-bs-dismiss="modal">Batal</button>
-                            <button type="submit" class="btn btn-primary">Simpan Data</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </section>
+        <!-- MODULE CREATE FORM MODAL -->
+        <module-create-modal v-bind:module-groups="groupList" v-bind:update-table="updateTable" v-bind:get-module-group="getModuleGroup"></module-create-modal>
 
-        <section id="edit-table">
-            <button ref="editFormButton" class="d-none" data-bs-toggle="modal" data-bs-target="#editFormModal"></button>
-            <div class="modal fade" id="editFormModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-                aria-labelledby="editFormModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                    <form v-on:submit.prevent="update" method="POST" class="modal-content">
-                        <div class="modal-header bg-primary text-white">
-                            <h1 class="modal-title fs-5" id="editFormModalLabel">Update Modul</h1>
-                        </div>
-                        <div class="modal-body">
-                            <div class="mb-3">
-                                <label for="moduleGroup" class="form-label fw-bold">
-                                    Grup
-                                    <span class="text-danger" title="Wajib Diisi">*</span>
-                                </label>
-                                <input v-model="updateData.group" list="groupList" type="text" class="form-control"
-                                    id="moduleGroup" name="group" maxlength="200" required>
-                                <datalist id="groupList">
-                                    <option v-for="(item, n) in groupList" v-bind:key="n" v-bind:value="item">
-                                        {{ item }}
-                                    </option>
-                                </datalist>
-                            </div>
-                            <div class="mb-3">
-                                <label for="moduleAlias" class="form-label fw-bold">
-                                    Alias
-                                    <span class="text-danger" title="Wajib Diisi">*</span>
-                                </label>
-                                <input v-model="updateData.alias" type="text" class="form-control" id="moduleAlias" name="alias" maxlength="100" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="moduleName" class="form-label fw-bold">
-                                    Nama
-                                    <span class="text-danger" title="Wajib Diisi">*</span>
-                                </label>
-                                <input v-model="updateData.name" type="text" class="form-control" id="moduleName" name="name" maxlength="200" autocomplete="off" required>
-                            </div>
-                            <div class="mb-3">
-                                <label for="moduleStatus" class="form-label fw-bold">
-                                    Status
-                                    <span class="text-danger" title="Wajib Diisi">*</span>
-                                </label>
-                                <select v-model="updateData.status" name="status" id="moduleStatus" class="form-select"
-                                    required>
-                                    <option value="Aktif">Aktif</option>
-                                    <option value="Nonaktif">Nonaktif</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button ref="updateModalCloseButton" type="button" class="btn btn-secondary"
-                                data-bs-dismiss="modal">Batal</button>
-                            <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </section>
+        <!-- MODULE UPDATE FORM MODAL -->
+        <module-update-modal ref="moduleUpdateModal" v-bind:module-groups="groupList" v-bind:update-table="updateTable" v-bind:get-module-group="getModuleGroup" v-bind:module-update-data="updateData"></module-update-modal>
 
         <section ref="moduleTableSection">
             <vue-table id="module-table" ref="moduleTable" v-bind:defaultLength="25" v-bind:lengthOptions="[10,25,50]"
                 v-bind:url="table.url" v-bind:order="table.order" v-bind:columns="table.columns"
                 v-bind:processData="processData" v-on:afterCreate="$emit('loaded')">
+
+                <!-- SLOT HEADER -->
                 <template v-slot:header>
                     <tr>
                         <th></th>
@@ -147,6 +36,45 @@
                         </th>
                     </tr>
                 </template>
+
+                <!-- SLOT ROW -->
+                <template v-slot:row="{ rowData, columnData, key }">
+                    <td v-bind:class="columnData[0].class">{{ rowData.no }}</td>
+                    <td v-bind:class="columnData[1].class">
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle table-dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fa-solid fa-list me-1"></i>
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <button v-on:click.prevent="editData(key)" class="edit-button dropdown-item" type="button" title="Edit Data">
+                                        <i class="fa-solid fa-pen-to-square me-1 text-primary"></i>
+                                        Edit
+                                    </button>
+                                </li>
+                                <li>
+                                    <button v-on:click.prevent="updateStatusRow(key)" v-bind:title="(rowData.status === 'Aktif') ? 'Nonaktifkan Data' : 'Aktifkan Data'" class="status-button dropdown-item" type="button">
+                                        <i v-bind:class="{ 'text-success': (rowData.status === 'Nonaktif'), 'text-warning': (rowData.status === 'Aktif') }" class="fa-solid fa-sliders me-1"></i>
+                                        {{ (rowData.status === 'Aktif') ? 'Nonaktifkan' : 'Aktifkan' }}
+                                    </button>
+                                </li>
+                                <li>
+                                    <button v-on:click.prevent="deleteRow(key)" class="delete-button dropdown-item" type="button" title="Hapus Data">
+                                        <i class="fa-solid fa-trash-can me-1 text-danger"></i>
+                                        Hapus
+                                    </button>
+                                </li>
+                            </ul>
+                        </div>
+                    </td>
+                    <td v-bind:class="columnData[2].class">{{ rowData.group }}</td>
+                    <td v-bind:class="columnData[3].class">{{ rowData.name }}</td>
+                    <td v-bind:class="columnData[4].class">{{ rowData.alias }}</td>
+                    <td v-bind:class="columnData[5].class">
+                        <span v-bind:class="{ 'bg-success': (rowData.status === 'Aktif'), 'bg-warning': (rowData.status === 'Nonaktif') }" class="text-white py-2 px-3 rounded-pill fw-bold">{{ rowData.status }}</span>
+                    </td>
+                </template>
+
             </vue-table>
         </section>
     </main>
@@ -156,6 +84,8 @@
 
 import { panelUrl, checkAxiosError, restructurized } from '@/libraries/Function'
 import VueTable from '@/libraries/Ghivarra/VueTable/VueTable.vue'
+import ModuleCreateModal from '../Modal/ModuleCreateModal.vue'
+import ModuleUpdateModal from '../Modal/ModuleUpdateModal.vue'
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
@@ -163,7 +93,9 @@ export default {
     name: 'panel-module-view',
     inject: ['showLoader', 'hideLoader'],
     components: {
-        'vue-table': VueTable
+        'vue-table': VueTable,
+        'module-create-modal': ModuleCreateModal,
+        'module-update-modal': ModuleUpdateModal
     },
     data: function() {
         return {
@@ -206,118 +138,30 @@ export default {
                     checkAxiosError(res.request.status)
                 })
         },
+        updateTable: function() {
+            this.$refs.moduleTable.draw()
+        },
         processData: function(data) {
-            this.tableData = data.row
-
-            if (data.row.length < 1) {
-                return data
-            }
-
-            data.row.forEach((item, i) => {
-                let btnText = (item.status === 'Aktif') ? 'Nonaktifkan' : 'Aktifkan'
-                let btnTextColor = (item.status === 'Aktif') ? 'text-warning' : 'text-success'
-                data.row[i].action = `<div class="dropdown">
-                                        <button class="btn btn-secondary dropdown-toggle table-dropdown" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                            <i class="fa-solid fa-list me-1"></i>
-                                        </button>
-                                        <ul class="dropdown-menu">
-                                            <li>
-                                                <button data-key="${i}" class="edit-button dropdown-item" type="button" title="Edit Data">
-                                                    <i class="fa-solid fa-pen-to-square me-1 text-primary"></i>
-                                                    Edit
-                                                </button>
-                                            </li>
-                                            <li>
-                                                <button data-key="${i}" class="status-button dropdown-item" type="button" title="${btnText} Data">
-                                                    <i class="fa-solid fa-sliders me-1 ${btnTextColor}"></i>
-                                                    ${btnText}
-                                                </button>
-                                            </li>
-                                            <li>
-                                                <button data-key="${i}" class="delete-button dropdown-item" type="button" title="Hapus Data">
-                                                    <i class="fa-solid fa-trash-can me-1 text-danger"></i>
-                                                    Hapus
-                                                </button>
-                                            </li>
-                                        </ul>
-                                    </div>`
-                data.row[i].groupDefault = item.group
-                data.row[i].statusDefault = item.status
-                data.row[i].group = `<p class="m-0 fw-bold">${item.group}</p>`
-                data.row[i].status = (item.status === 'Aktif') ? `<span class="bg-success text-white py-2 px-3 rounded-pill fw-bold">${item.status}</span>` : `<span class="bg-warning py-2 px-3 text-white rounded-pill fw-bold">${item.status}</span>`
-            })
-
+            this.tableData = (data.row.length < 1) ? [] : [... data.row]
             return data
         },
         editData: function(key) {
             let item = restructurized(this.tableData[key])
 
             this.updateData.id = parseInt(item.id)
-            this.updateData.group = item.groupDefault
+            this.updateData.group = item.group
             this.updateData.alias = item.alias
             this.updateData.name = item.name
-            this.updateData.status = item.statusDefault
-            this.$refs.editFormButton.click()
+            this.updateData.status = item.status
+            this.$refs.moduleUpdateModal.$refs.modalOpenButton.click()
         },
-        create: function(e) {
-            let form = e.target
-            let app = this
-
-            // show loader
-            app.showLoader()
-
-            // save data
-            axios.post(panelUrl('module/create'), new FormData(form))  
-                .then(function(res) {
-                    res = res.data
-                    app.hideLoader()
-                    if (res.status !== 'success') {
-                        Swal.fire('Whoopss!!', res.message, 'warning')
-                    } else {
-                        form.reset()
-                        app.$refs.createModalCloseBtn.click()
-                        app.getModuleGroup()
-                        app.$refs.moduleTable.draw()
-                    }
-                }).catch(function(res) {
-                    app.hideLoader()
-                    checkAxiosError(res.request.status)
-                })
-        },
-        update: function(e) {
-            let form = new FormData(e.target)
-            let app = this
-
-            form.append('id', app.updateData.id)
-
-            // show loader
-            app.showLoader()
-
-            // save data
-            axios.post(panelUrl('module/update'), form)  
-                .then(function(res) {
-                    res = res.data
-                    app.hideLoader()
-                    if (res.status !== 'success') {
-                        Swal.fire('Whoopss!!', res.message, 'warning')
-                    } else {
-                        e.target.reset()
-                        app.$refs.updateModalCloseButton.click()
-                        app.getModuleGroup()
-                        app.$refs.moduleTable.draw()
-                    }
-                }).catch(function(res) {
-                    app.hideLoader()
-                    checkAxiosError(res.request.status)
-                })
-        },
-        updateStatus: function(key) {
+        updateStatusRow: function(key) {
             this.showLoader()
 
             // set status
             let app = this
-            let data = app.tableData[key]
-            let targetStatus = (data.statusDefault === 'Aktif') ? 'Nonaktif' : 'Aktif'
+            let data = restructurized(app.tableData[key])
+            let targetStatus = (data.status === 'Aktif') ? 'Nonaktif' : 'Aktif'
             
             // create form
             let form = new FormData()
@@ -339,12 +183,12 @@ export default {
                     checkAxiosError(res.request.status)
                 })
         },
-        delete: function(key) {
+        deleteRow: function(key) {
             this.showLoader()
 
             // set status
             let app = this
-            let data = app.tableData[key]
+            let data = restructurized(app.tableData[key])
 
             // create form
             let form = new FormData()
@@ -372,25 +216,8 @@ export default {
     mounted: function() {
         let app = this
 
-        app.$refs.moduleTableSection.addEventListener('click', (event) => {
-            event.preventDefault()
-            if (event.target.closest('.edit-button')) {
-                app.editData(event.target.getAttribute('data-key'))
-            }
-        })
-
-        app.$refs.moduleTableSection.addEventListener('click', (event) => {
-            event.preventDefault()
-            if (event.target.closest('.status-button')) {
-                app.updateStatus(event.target.getAttribute('data-key'))
-            }
-        })
-
-        app.$refs.moduleTableSection.addEventListener('click', (event) => {
-            event.preventDefault()
-            if (event.target.closest('.delete-button')) {
-                app.delete(event.target.getAttribute('data-key'))
-            }
+        app.$nextTick(() => {
+            app.$emit('loaded')
         })
     }
 }
