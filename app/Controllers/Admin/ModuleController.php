f8224f6d67b10165797c7b16f35e471d0c6151c6
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2024-11-26 12:23:37 +0700

    add session_write_close on heavy-AJAX usage on data getter functions and datatable inside Module Controller

--- app/Controllers/Admin/ModuleController.php
+++ app/Controllers/Admin/ModuleController.php
@@ -93,6 +93,9 @@ class ModuleController extends BaseController
             return cannotAccessModule();
         }
 
+        // session not needed anymore, unlock the session file mechanism
+        session_write_close();
+
         // create model instance
         $orm = new AdminModuleModel();
 
@@ -209,6 +212,9 @@ class ModuleController extends BaseController
             return cannotAccessModule();
         }
 
+        // session not needed anymore, unlock the session file mechanism
+        session_write_close();
+
         // create ORM instance
         $orm  = new AdminModuleModel();
         $data = $orm->where('alias', $this->request->getGet('alias'))->first();
@@ -240,6 +246,9 @@ class ModuleController extends BaseController
         {
             return cannotAccessModule();
         }
+
+        // session not needed anymore, unlock the session file mechanism
+        session_write_close();
 
         // create ORM instance
         $orm  = new AdminModuleModel();
