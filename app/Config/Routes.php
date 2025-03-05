b8828e0c1dc4733940306b58ba6da7d9c1a7a62d
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2025-03-05 18:14:19 +0700

    add /version routes to get all kind of versioning

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/version', 'Home::version');
 
 // login routes
 $routes->group($_ENV['LOGIN_PAGE'], static function($routes) {
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,6 +2,8 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\HTTP\Response;
+use CodeIgniter\CodeIgniter;
 use App\Models\WebsiteModel;
 
 class Home extends BaseController
@@ -11,5 +13,24 @@ class Home extends BaseController
         return 'Davion Panel is working as intended.';
     }
     
+    //================================================================================================
+
+    public function version(): Response
+    {
+        // load orm
+        $orm = new WebsiteModel();
+
+        // load all data
+        $data = $orm->getAllData();
+
+        // return
+        return response()->setJSON([
+            'codeigniter_version' => CodeIgniter::CI_VERSION,
+            'app_version'         => $data['app_version'],
+            'icon_version'        => $data['icon_version'],
+            'logo_version'        => $data['logo_version'],
+        ]);
+    }
+
     //================================================================================================
 }
