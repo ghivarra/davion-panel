86ffafd5d1313db9fb16ef48397599f2c0fb363c
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2025-03-05 18:36:50 +0700

    bump CodeIgniter Version from 4.5.5 to 4.6.0

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -36,7 +36,7 @@ abstract class BaseController extends Controller
      * class instantiation. These helpers will be available
      * to all other controllers that extend BaseController.
      *
-     * @var array
+     * @var list<string>
      */
     protected $helpers = [];
 
@@ -59,7 +59,7 @@ abstract class BaseController extends Controller
 
         // Preload any models, libraries, etc, here.
 
-        // E.g.: $this->session = \Config\Services::session();
+        // E.g.: $this->session = service('session');
     }
 
     // create role check functions on base controller
